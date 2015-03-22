87d724fb6adc03cd0eaf9cc78fd6c3be9a49312f
Juul Arthur Ribe Rudihagen <juularthur92@gmail.com>
2015-03-22 21:49:03 +0100

    add more portfolios. Need to add links to more images and fix weird loading

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -3,24 +3,47 @@ angular.module('maginationApp').config(['$routeProvider', '$locationProvider', (
     $locationProvider.html5Mode(true);
 
     $routeProvider
-
         .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'mainController'
-    }).when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'loginController'
-    }).when('/forum', {
-        templateUrl: 'views/forum.html',
-        controller: 'forumController'
-    }).when('/portfolio/1', {
-        templateUrl: 'projects/project-1.html',
-        controller: 'mainController'
-    }).when('/portfolio/2', {
+            templateUrl: 'views/home.html',
+            controller: 'mainController'
+        }).when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'loginController'
+        }).when('/forum', {
+            templateUrl: 'views/forum.html',
+            controller: 'forumController'
+        }).when('/portfolio/1', {
+            templateUrl: 'projects/project-1.html',
+            controller: 'mainController'
+        }).when('/portfolio/2', {
             templateUrl: 'projects/project-2.html',
             controller: 'mainController'
-    }).otherwise({
-        templateUrl: 'views/home.html',
-        controller: 'mainController'
-    });
+        }).when('/portfolio/3', {
+            templateUrl: 'projects/project-3.html',
+            controller: 'mainController'
+        }).when('/portfolio/4', {
+            templateUrl: 'projects/project-4.html',
+            controller: 'mainController'
+        }).when('/portfolio/5', {
+            templateUrl: 'projects/project-5.html',
+            controller: 'mainController'
+        }).when('/portfolio/6', {
+            templateUrl: 'projects/project-6.html',
+            controller: 'mainController'
+        }).when('/portfolio/7', {
+            templateUrl: 'projects/project-7.html',
+            controller: 'mainController'
+        }).when('/portfolio/8', {
+            templateUrl: 'projects/project-8.html',
+            controller: 'mainController'
+        }).when('/portfolio/9', {
+            templateUrl: 'projects/project-9.html',
+            controller: 'mainController'
+        }).when('/portfolio/10', {
+            templateUrl: 'projects/project-10.html',
+            controller: 'mainController'
+        }).otherwise({
+            templateUrl: 'views/home.html',
+            controller: 'mainController'
+        });
 })]);
